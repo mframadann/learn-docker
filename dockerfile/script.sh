24d610067e4b8079fd38a62d7a697e27d8ffe01a
Muhamad Firly Ramadan <dev.ramadann@gmail.com>
2024-04-30 15:07:17 +0700

    dockerfile: run✨

--- dockerfile/script.sh
+++ dockerfile/script.sh
@@ -1,2 +1,6 @@
 # From instruction
 docker build -t lydev/from from
+
+#Run instruction
+docker build -t lydev/run run
+docker build -t lydev/run run --progress=plain --no-cache ## Run with inspect output
