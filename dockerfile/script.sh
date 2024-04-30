894bd36b2e3e8a47a229b2b0af2229a2a4e7b85e
Muhamad Firly Ramadan <dev.ramadann@gmail.com>
2024-04-30 15:41:48 +0700

    dockerfile: cmd✨

--- dockerfile/script.sh
+++ dockerfile/script.sh
@@ -3,4 +3,13 @@ docker build -t lydev/from from
 
 #Run instruction
 docker build -t lydev/run run
-docker build -t lydev/run run --progress=plain --no-cache ## Run with inspect output
+docker build -t lydev/run run --progress=plain --no-cache ## Run with inspect output without cache
+
+# Command instruction
+docker build -t lydev/cmd cmd
+
+docker container create --name "cmd" lydev/cmd # Create container from image lydev/cmd
+docker container start cmd # Start container cmd
+docker container logs cmd # See the result of CMD instruction
+
+# Label instruction
